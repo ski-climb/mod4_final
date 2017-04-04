bdaf169df33028c24da8a9b5c497c0b61b37bec1
Nicholas Erhardt <ski.climb.europe@gmail.com>
2017-04-04 13:02:08 -0600

    User can successfully log out

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
 
   get "/login" => "sessions#new"
   post "/login" => "sessions#create"
+  delete "/logout" => "sessions#destroy"
 
   resources :links, only: [:index]
   resources :users, only: [:new, :create] do
