721251dcc0fa62b65c74b9db06d5fab32d7f65c2
Nicholas Erhardt <ski.climb.europe@gmail.com>
2017-04-04 16:31:14 -0600

    User has many links

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,4 +5,6 @@ describe User do
   it { is_expected.to validate_uniqueness_of(:email) }
   it { is_expected.to validate_presence_of(:password) }
   it { is_expected.to have_secure_password }
+
+  it { is_expected.to have_many(:links) }
 end
