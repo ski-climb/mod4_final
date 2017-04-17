ec14698d8bab5742c35d7db190bdba69abc0d11c
Nicholas Erhardt <ski.climb.europe@gmail.com>
2017-04-17 10:51:04 -0600

    Failing spec for marking link as read

--- spec/features/mark_as_read_spec.rb
+++ spec/features/mark_as_read_spec.rb
@@ -1,18 +1,23 @@
 require "rails_helper"
 
 RSpec.describe "can mark links as read", :js => :true do
-  xit "Mark a link as read" do
-    Link.create(url:"https://turing.io", title:"Turing")
-    visit "/"
-    within('.link .read-status') do
-      expect(page).to have_text("false")
+  context "User is logged in" do
+    let!(:user) { create(:user) }
+    let!(:link) { create(:link, user: user) }
+    before { log_in_as_user(user) }
+
+    it "Mark a link as read" do
+      visit "/"
+      within('.link .read-status') do
+        expect(page).to have_text("false")
+      end
+
+      click_on "Mark as Read"
+
+      within('.link .read-status') do
+        expect(page).to have_text("true")
+      end
+
     end
-
-    click_on "Mark as Read"
-
-    within('.link .read-status') do
-      expect(page).to have_text("true")
-    end
-
   end
 end
