f2cd9c4f42a2e7a6707c3e6a38015d592b82e2bf
Nicholas Erhardt <ski.climb.europe@gmail.com>
2017-04-17 09:49:47 -0600

    Better to redirect user to sign in than render 404

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -4,6 +4,5 @@ class LinksController < ApplicationController
   def index
     @link = Link.new
     @links = current_user.links.most_recent
-    render file: 'public/404' unless current_user
   end
 end
