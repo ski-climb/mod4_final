2b91fbc7844914e656c50c205fcca40f1cab35a1
Nicholas Erhardt <ski.climb.europe@gmail.com>
2017-04-17 09:49:03 -0600

    Complete failing spec for creating links

--- spec/features/links/create_spec.rb
+++ spec/features/links/create_spec.rb
@@ -12,14 +12,18 @@ describe "Creating links", :js => true do
       fill_in "Title", with: ""
       click_on "Add Link"
 
-      expect(page).to have_content "cats" 
-      expect(page).to have_content "dogs" 
+      expect(page).to have_content "Url can't be blank"
+      expect(page).to have_content "Title can't be blank"
+      expect(Link.count).to eq 0
     end
 
     it "does not create a new link when the url is invalid" do
-      fail
+      fill_in "URL", with: "pretty sure this isn't legit"
+      fill_in "Title", with: "It looked OK to me"
+      click_on "Add Link"
+
+      expect(page).to have_content "Url is not a valid URL"
+      expect(Link.count).to eq 0
     end
-
-
   end
 end
