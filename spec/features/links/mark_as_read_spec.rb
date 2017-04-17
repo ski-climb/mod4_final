63f44baf3165047a0adc981e8c3d487bdbaa34bd
Nicholas Erhardt <ski.climb.europe@gmail.com>
2017-04-17 13:21:20 -0600

    Finish failing spec for user marking link as read

--- spec/features/links/mark_as_read_spec.rb
+++ spec/features/links/mark_as_read_spec.rb
@@ -4,20 +4,23 @@ RSpec.describe "can mark links as read", :js => :true do
   context "User is logged in" do
     let!(:user) { create(:user) }
     let!(:link) { create(:link, user: user) }
-    before { log_in_as_user(user) }
+    before do
+      log_in_as_user(user)
+      visit root_path
+    end
 
     it "Mark a link as read" do
-      visit "/"
+      expect(page).to have_content("Mark as Read")
       within('.link .read-status') do
-        expect(page).to have_text("false")
+        expect(page).to have_content("false")
       end
 
       click_on "Mark as Read"
 
+      expect(page).to have_content("Mark as Unread")
       within('.link .read-status') do
-        expect(page).to have_text("true")
+        expect(page).to have_content("true")
       end
-
     end
   end
 end
