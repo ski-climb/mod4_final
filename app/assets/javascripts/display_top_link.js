756c86612891426ba19c6826fb35b18d074825eb
Nicholas Erhardt <ski.climb.europe@gmail.com>
2017-04-17 22:18:05 -0600

    Style Top and Hot links

--- app/assets/javascripts/display_top_link.js
+++ app/assets/javascripts/display_top_link.js
@@ -1,26 +1,39 @@
 $(document).ready(function() {
+
+  showTopLink();
+  showHotLinks();
+
+});
+
+function showTopLink() {
   $.ajax({
     url: "http://localhost:2000/api/v1/links/top_link",
     method: "GET"
   })
   .success(function(data) {
-    $(`[data-url="${data}"]`).prepend("Top Link!");
+    $(`[data-url="${data}"]`)
+      .prepend("Top Link!")
+      .addClass('top_link');
   })
   .fail(function() {
     console.log('no top link');
   })
+}
 
+
+function showHotLinks() {
   $.ajax({
     url: "http://localhost:2000/api/v1/links/hot_links",
     method: "GET"
   })
   .success(function(data) {
     for(var i=0; i<data.length; i++) {
-      $(`[data-url="${data[i]}"]`).prepend("Hot Link!");
+      $(`[data-url="${data[i]}"]`)
+        .prepend("Hot Link!")
+        .addClass('hot_link');
     }
   })
   .fail(function() {
     console.log('no hot links');
   })
-});
-
+}
